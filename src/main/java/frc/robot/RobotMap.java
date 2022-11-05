2c1d537961634716f1dbf38322cd3e309e2bd67e
gabetprhs <gabe.t@prhsrobotics.com>
2022-11-05 11:15:26 -0400

    Initial commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,17 @@
+public class RobotMap {
+ 
+  public static final int LEFT_FRONT_DRIVE_METHOD = 3;
+  public static final int RIGHT_FRONT_DRIVE_PORT=4;  
+  public static final int LEFT_BACK_DRIVE_PORT = 2;
+  public static final int RIGHT_BACK_DRIVE_PORT = 2;
+  public static WPI_TalonFX leftFrontDriveMotor = new WPI_TalonFx(LEFT_FRONT_DRIVE_PORT);
+  public static WPI_TalonFX rightFrontDriveMotor = new WPI_TalonFx(RIGHT_FRONT_DRIVE_PORT);
+  public static WPI_TalonFX leftBackDriveMotor = new WPI_TalonFX (LEFT_BACK_DRIVE_PORT )
+  public static WPI_TalonFX leftBackDriveMotor = new WPI_TalonFX (RIGHT_BACK_DRIVE_PORT )
+  public static DriveSubsytem drivesubsytem = new DriveSubsystem
+  public static final XboxController driveControlle = neew XboxController driveController = new XboxController(0);
+   
+
+
+
+}
